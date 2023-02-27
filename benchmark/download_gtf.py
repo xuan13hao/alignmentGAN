731d1ab55995fcb825bc1bcd6b6017efdaf185b6
xuan13hao <xuan@DESKTOP-3IDD8L2>
2023-02-27 14:03:52 -0600

    rmduplicatescript

--- benchmark/download_gtf.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-url = 'https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/genes/hg19.refGene.gtf.gz'
-filename = 'hg19.refGene.gtf.gz'
-
-# Download the file from the URL
-urllib.request.urlretrieve(url, filename)
-
-print('Download complete.')
